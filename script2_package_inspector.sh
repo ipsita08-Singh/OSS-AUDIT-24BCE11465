6a54d6c5efc1be9442e8722facc3d993b5a1994a
ipsita08-Singh <ipsita.24bce11465@vitbhopal.ac.in>
2026-03-30 20:57:21 +0530

    Update script2_package_inspector.sh

--- script2_package_inspector.sh
+++ script2_package_inspector.sh
@@ -1,20 +1,27 @@
-@@ -0,0 +1,19 @@
 #!/bin/bash
-# Script 2: FOSS Package Inspector
+pkg="git"
 
-PACKAGE="git"
-
-if dpkg -l | grep -qw $PACKAGE; then
-    echo "$PACKAGE is installed."
-    apt show $PACKAGE 2>/dev/null | grep -E 'Version|Maintainer|Description'
+if dpkg-query -W -f='${Status}' "$pkg" 2>/dev/null | grep -q "installed"; then
+    echo "$pkg is installed."
+    
+    apt show "$pkg" 2>/dev/null | awk -F': ' '/Version|Maintainer|Description/ {print $1": "$2}'
 else
-    echo "$PACKAGE is NOT installed."
+    echo "$pkg is NOT installed."
 fi
-
-case $PACKAGE in
-    git) echo "Git: decentralized version control built for developers" ;;
-    apache2) echo "Apache: backbone of the open web" ;;
-    mysql) echo "MySQL: open source database powering applications" ;;
-    firefox) echo "Firefox: privacy-focused open web browser" ;;
-    *) echo "Unknown package" ;;
+case "$pkg" in
+    git)
+        echo "Git: decentralized version control built for developers"
+        ;;
+    apache2)
+        echo "Apache: backbone of the open web"
+        ;;
+    mysql)
+        echo "MySQL: open source database powering applications"
+        ;;
+    firefox)
+        echo "Firefox: privacy-focused open web browser"
+        ;;
+    *)
+        echo "Unknown package"
+        ;;
 esac
